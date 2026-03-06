c064275305dac17c10c1116c06893871c84f655c
tournesol59 <frederic.peugny@example.com>
2026-03-06 16:37:01 +0100

    just the __init__ configurator, next do the database

---
+++ flaskr/Book.py
@@ -0,0 +1,9 @@
+class Book:
+   def __init__(self, _data):
+      #initialize
+	  self.title = _data['title']
+	  self.author = _data['author']
+	  self.categ = _data['category']
+	  self.heading = _data['heading']
+	  
+	  

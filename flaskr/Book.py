c6bf54f778980475e98f0fde00032b4eeeacc53d
tournesol59 <frederic.peugny@example.com>
2026-03-22 23:56:47 +0100

    compiled, server starts but not tested

--- flaskr/Book.py
+++
@@ -1,9 +0,0 @@
-class Book:
-   def __init__(self, _data):
-      #initialize
-	  self.title = _data['title']
-	  self.author = _data['author']
-	  self.categ = _data['category']
-	  self.heading = _data['heading']
-	  
-	  
